5d43eb14c5443a840fc516baa0d1680d3367bae8
Sean <seanpai96@gmail.com>
2021-03-10 17:38:28 +0800

    java

--- Java/IDEA/src/seanpai/java/Main.java
+++ Java/IDEA/src/seanpai/java/Main.java
@@ -1,40 +1,15 @@
 package seanpai.java;
 
+
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        Student sean = new Student();
-        sean.height = 175;
-        sean.weight = 75;
-        sean.chinese.score = 50;
-
-	    System.out.println("Sean's Chinese score is " + sean.chinese.score + "!\n");
-	    if(sean.isFail()){
-            System.out.println("Sean failed Chinese!");
+        Random rand = new Random();
+        for(int i=0;i<100;i++){
+            int x = rand.nextInt(40);
+            System.out.println(x);
         }
-    }
-
-
-    public static class Student{
-        int height;
-        int weight;
-        Subject chinese = new Subject();
-
-        public void setChinese(Subject chinese) {
-            this.chinese = chinese;
-        }
-
-        private boolean isFail(){
-            if(chinese.score < 60){
-                return true;
-            }else{
-                return false;
-            }
-        }
-
-    }
-
-    public static class Subject{
-        int score;
     }
 }
